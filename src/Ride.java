ba924362a6dd84af2ec22255c5ee42e5b391e5ca
Tzy <1489354119>
2024-12-13 17:04:11 +0800

    Preliminary completion of class Visitor and class  ...

--- src/Ride.java
+++ src/Ride.java
@@ -1,3 +1,42 @@
 public class Ride {
+    private int rideID;
+    private String ridename;
+    private Employee rideOper;
+    private boolean isOpen;
+    
+    public Ride(){
+        rideID=0;
+        ridename="";
 
+    }
+    public Ride(int rideID,String ridename,Employee rideOper,boolean isOpen){
+        this.rideID=rideID;
+        this.ridename=ridename;
+        this.isOpen=isOpen;
+        this.rideOper=rideOper;
+    }
+    public void setrideID(int rideID){
+        this.rideID=rideID;
+    }
+    public int getrideID(){
+        return rideID;
+    }
+    public void  setridename(String ridename){
+        this.ridename=ridename;
+    }
+    public String getridename(){
+        return ridename;
+    }
+    public void setrideOper(Employee rideOper){
+        this.rideOper=rideOper;
+    }
+    public Employee getrideOper(){
+        return rideOper;
+    }
+    public void setisOpen(boolean isOpen){
+        this.isOpen=isOpen;
+    }
+    public boolean getisOpen(){
+        return isOpen;
+    }
 }
--- src/Visitor.java
+++ src/Visitor.java
@@ -1,3 +1,26 @@
 public class Visitor extends Person{
+    private String city;
+    private int visitornum;
 
+    public Visitor(){
+       super();
+    }
+    
+    public Visitor(String name,int age,String gender,String city,int visitornum){
+        super(name,age,gender);
+        this.city=city;
+        this.visitornum=visitornum;
+    }
+    public void setCity(String city){
+        this.city=city;
+    }
+    public String getCity(){
+        return city;
+    }
+    public  void setVisitornum(int visitornum){
+        this.visitornum=visitornum;
+    }
+    public int getVisitornum(){
+        return visitornum;
+    }
 }
